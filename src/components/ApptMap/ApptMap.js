d8d1dc93e1673f1a1804c58f78aa3ac16462de7d
TJ Hindman <thind131@gmail.com>
2021-08-25 22:08:50 -0400

    temporary fix for api key build error

--- src/components/ApptMap/ApptMap.js
+++ src/components/ApptMap/ApptMap.js
@@ -49,7 +49,7 @@ export const ApptMap = () => {
       </div>
       <div className="mapWrap">
         <Map
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAWyzFI27X__JRZ_68V_i07rb--MQigIYA`}
           loadingElement={<div style={{ height: "100%" }} />}
           containerElement={<div style={{ height: "100%" }} />}
           mapElement={<div style={{ height: "100%" }} />}
