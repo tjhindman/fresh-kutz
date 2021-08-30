19725d12b5580ef4bcb93d98cb64e34aa51856c6
TJ Hindman <thind131@gmail.com>
2021-08-30 11:45:29 -0400

    meta tags moved to html for quick fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Helmet } from "react-helmet";
 
 import { Ad, Header, ApptMap, ServicesHours, Footer } from "./components";
 
@@ -8,22 +7,6 @@ import "./App.css";
 function App() {
   return (
     <React.Fragment>
-      <Helmet htmlAttributes={{ lang: "en" }}>
-        <title>Quality Haircuts in Indianapolis | Fresh Kutz Barbershop</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta
-          name="description"
-          content="Best barbershop in Indianapolis. 
-          Fresh Kutz offers quality haircut services paired with great vibes and great barbers. 
-          Online appointments available for line ups, haircuts, and other haircut services. 
-          Get an appointment scheduled and come get fresh!"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          name="google-site-verification"
-          content="tQLf2DLkDPFHMVZ-tOw6SI6vRMcZod_QUBL9_WpzzvY"
-        />
-      </Helmet>
       <Ad />
       <Header />
       <ApptMap />
